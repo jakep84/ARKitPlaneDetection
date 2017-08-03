eae2f191831efc6ef2e49ad4044d6ea008bb0512
Jake Priddy <priddyjacob84@gmail.com>
2017-08-03 13:21:38 -0400

    Initial Commit

---
+++ planeDetection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  planeDetection
+//
+//  Created by Jake Priddy on 8/3/17.
+//  Copyright © 2017 Jake Priddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ planeDetection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  planeDetection
+//
+//  Created by Jake Priddy on 8/3/17.
+//  Copyright © 2017 Jake Priddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
